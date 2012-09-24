8eacc73dc6dcdbf9d096803c08945e65404ab7e1
Ricardo Castro <mcc.ricardo@gmail.com>
2012-09-24 19:25:19 +0100

    Possible prime list

--- sieve_of_atkin.py
+++ sieve_of_atkin.py
@@ -3,6 +3,6 @@ class SieveOfAtkin:
 		self.limit = limit
 
 	def getPrimes(self):
-		primes = []
-		
+		primes = [(2,1), (3,1), (5,1)]
+		primes += [(x,0) for x in range (6,limit) if x%2!=0]
 		return primes
